4a2f946aab6dfc41ef7c3552d343621cc8feb0aa
Luciano Freitas <lucsilfreitas@gmail.com>
2020-09-10 16:22:51 -0300

    @value2

--- algafood-api/src/main/java/com/algaworks/algafood/domain/repository/CustomJpaRepository.java
+++ algafood-api/src/main/java/com/algaworks/algafood/domain/repository/CustomJpaRepository.java
@@ -9,6 +9,6 @@ import org.springframework.data.repository.NoRepositoryBean;
 @NoRepositoryBean
 public interface CustomJpaRepository<T, ID> extends JpaRepository<T, ID> {
 
-	Optional<T> buscarPrimeiro();
+	//Optional<T> buscarPrimeiro();
 	
 }
